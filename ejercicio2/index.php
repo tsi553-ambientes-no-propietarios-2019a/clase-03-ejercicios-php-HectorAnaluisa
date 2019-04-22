12dc25544bddfb9ffe5f2947286595a5e0da6c09
HectorAnaluisa <hector.analuisa@epn.edu.ec>
2019-04-22 04:15:29 -0500

    Titulo centrado

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -2,6 +2,16 @@
 /**
  * Problema propuesto:
  *
+ * Héctor Analuisa
+ * 
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
+function fornais (){
+    
+    echo '<H1 align= "center"> TITULO CENTRADO EN PANTALLA </H1>';
+}
+
+fornais(); 
+fornais(); 
+fornais(); 
 ?>
