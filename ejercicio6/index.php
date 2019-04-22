ba5350e8439a1b00e8399b91fc636abf3574f2bb
HectorAnaluisa <hector.analuisa@epn.edu.ec>
2019-04-22 05:29:56 -0500

    Busqueda de una vocal informacion si no

--- ejercicio6/index.php
+++ ejercicio6/index.php
@@ -16,7 +16,7 @@ http://buscapalabras.com.ar/palabras-con-las-cinco-vocales.html
  //Héctor Analuisa 
 
  function buscaVocales(){
-    $shalala = 'Ministerio';
+    $shalala = 'Murcielago';
     $buscaA = substr_count($shalala, 'a') + substr_count($shalala, 'A'); 
     $buscaE = substr_count($shalala, 'e') + substr_count($shalala, 'E'); 
     $buscaI = substr_count($shalala, 'i') + substr_count($shalala, 'I'); 
@@ -26,16 +26,23 @@ http://buscapalabras.com.ar/palabras-con-las-cinco-vocales.html
     echo "EL NUMERO TOTAL DE CARACTERES DE LA PALABRA ( $shalala ) es: $numeroTOTAL"; 
     echo "<br>"; 
     if($buscaA == 1 and $buscaE == 1 and $buscaI == 1 and $buscaO == 1 and $buscaU == 1){
-        echo "La palabra ($shalala) contiene todas las vocales"; 
+        echo "La palabra ($shalala) contiene todas las vocales una sola vez"; 
     }else{
-        echo "La palabra ($shalala) NO contiene todas las vocales :(";
+        echo "La palabra ($shalala) NO contiene todas las vocales o tiene más de una vocal repetida";
+        echo "<br>";
+        echo 'Informe: '; 
+        echo "<br>";
+        echo "Numero de vocales A: $buscaA <br>";
+        echo "Numero de vocales E: $buscaE <br>";
+        echo "Numero de vocales I: $buscaI <br>";
+        echo "Numero de vocales O: $buscaO <br>";
+        echo "Numero de vocales U: $buscaU <br>";
     }
+
 }
     echo "<br>";
     buscaVocales(); 
     echo "<br>";
-
-?>
 
 
 
